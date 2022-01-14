a0c805e3eba6df82bcfdcf1a2fd0ce20551ac8d6
Unknown <deledwards@comcast.net>
2022-01-14 14:08:05 -0500

    temp fix for table

--- ZipCodeFinder/ZipCodeListViewController.swift
+++ ZipCodeFinder/ZipCodeListViewController.swift
@@ -11,6 +11,10 @@ import SwiftyJSON
 import Alamofire
 
 class ZipCodeListController: UITableViewController {
+
+    static let zipCodeListCell = "ZipCodeListCell"
+
+
     
     let disposeBag = DisposeBag()
     
@@ -23,6 +27,8 @@ class ZipCodeListController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 
         print("zip : \(zip ?? "not set")")
         print("distance : \(distance ?? "not set")")
@@ -39,6 +45,11 @@ class ZipCodeListController: UITableViewController {
         //getData(zip: unZip, distance: unDist)
 
         zipcodeList = prepareData()
+
+//        tableView.dataSource = self
+//        tableView.delegate = self
+        tableView.reloadData()
+
 
         print(zipcodeList as AnyObject)
     }
@@ -162,71 +173,36 @@ class ZipCodeListController: UITableViewController {
         
     }
 
+   
+
+}
+
+extension ZipCodeListController {
+
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return zipcodeList?.count ?? 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.zipCodeListCell, for: indexPath) as? ZipCodeListCell else {
+            fatalError("Unable to dequeue ZipCodeListCell")
+        }
 
-        // Configure the cell...
+        let zipcode = zipcodeList?[indexPath.row]
+//        cell.cityLabel.text = zipcode?.city
+//        cell.stateLabel.text = zipcode?.state
+//        cell.distanceLabel.text = zipcode?.distance.description
+        cell.zipLabel.text = zipcode?.zipCode
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
